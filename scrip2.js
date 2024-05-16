4da7f80cae45bfe4654a398bcc68a1639961b141
Bottleneck14 <daffarere14@gmail.com>
2024-05-16 12:06:21 +0700

    perubahan

--- scrip2.js
+++ scrip2.js
@@ -1,9 +1,6 @@
-// Dapatkan elemen hamburger dan navigasi mobile
 var hamburger = document.querySelector(".hamburger");
 var navMobile = document.querySelector(".navigasi-mobile");
 
-// Tambahkan event listener ke hamburger
 hamburger.addEventListener("click", function () {
-  // Toggle kelas 'show' pada navigasi mobile
   navMobile.classList.toggle("show");
 });
--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-// Array berisi alamat gambar
 var gambar = ["./src/bg3.png", "./src/bg2.jpeg", "./src/spot2.jpg"];
 
-// Variabel untuk melacak indeks gambar saat ini
 var indeksSaatIni = 0;
 
-// Fungsi untuk mengubah background
 function ubahBackground() {
-  // Dapatkan elemen dengan class 'image'
   var elemen = document.querySelector(".image");
 
-  // Ubah background elemen
   elemen.style.backgroundImage = "url(" + gambar[indeksSaatIni] + ")";
 }
 
-// Fungsi untuk maju ke gambar berikutnya
 function maju() {
   indeksSaatIni = (indeksSaatIni + 1) % gambar.length;
   ubahBackground();
 }
 
-// Fungsi untuk kembali ke gambar sebelumnya
 function mundur() {
   indeksSaatIni = (indeksSaatIni - 1 + gambar.length) % gambar.length;
   ubahBackground();
 }
 
-// Dapatkan tombol dan tambahkan event listener
 var tombolKanan = document.querySelector(".kanan");
 tombolKanan.addEventListener("click", maju);
 
@@ -44,12 +36,9 @@ tombol.forEach(function (elem) {
 });
 
 // _________________________________________________________________________________
-// Dapatkan elemen hamburger dan navigasi mobile
 var hamburger = document.querySelector(".hamburger");
 var navMobile = document.querySelector(".navigasi-mobile");
 
-// Tambahkan event listener ke hamburger
 hamburger.addEventListener("click", function () {
-  // Toggle kelas 'show' pada navigasi mobile
   navMobile.classList.toggle("show");
 });
